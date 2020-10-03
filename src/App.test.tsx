da231f749a13acd4d28f6bdb11dfecbd0ddc7c0f
Benny <carlsson.benny93@gmail.com>
2020-10-03 19:54:42 +0200

    chore: setup tests and styled componenets

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import { render } from "@testing-library/react"
+import renderer from "react-test-renderer"
+import App from "./App"
+import { shallow } from "enzyme"
+import logo from "./logo.svg"
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it("renders learn react link", () => {
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+it("render App component", () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it("render an image", () => {
+  const wrapper = shallow(<App />)
+  wrapper.contains(<img src={logo} className="App-logo" alt="logo" />)
+})
